3417ee9c9ff6b110f60f2dbda0ea6fbb081b7560
Kanalgundeli <168670361+Kanalgundeli@users.noreply.github.com>
2024-05-19 13:11:24 +0530

    Update indec1.js

--- client/src/api/indec1.js
+++ client/src/api/indec1.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //const API = axios.create({ baseURL: 'http://localhost:5000'});
-const API = axios.create({ baseURL: 'https://stackoverflowbackend-oucf.onrender.com'});
+const API = axios.create({ baseURL: 'https://project-stackoverflow-5.onrender.com/'});
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('Profile')){
